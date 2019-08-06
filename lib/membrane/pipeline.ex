e36a822530a416dae1b90570827a99aabc5c9862
Dominik Stanaszek <dominik.stanaszek@swmansion.com>
2019-08-06 14:53:19 +0200

    Use common spec module between Bin/Pipeline

--- lib/membrane/pipeline.ex
+++ lib/membrane/pipeline.ex
@@ -26,6 +26,11 @@ defmodule Membrane.Pipeline do
   use Membrane.Core.PlaybackHandler
   use Membrane.Core.PlaybackRequestor
 
+  
+  defmodule Spec do
+    use Membrane.Core.ParentSpec
+  end
+
   @typedoc """
   Defines options that can be passed to `start/3` / `start_link/3` and received
   in `c:handle_init/1` callback.
